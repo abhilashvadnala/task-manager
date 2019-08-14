c47b83c31cb4097ae0d2b5755ed491abcd9cff31
abhilash <abhivadnala@gmail.com>
2019-08-14 00:09:03 -0700

    starting to create Resource Endpoints

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require(`mongoose`)
+const validator = require(`validator`)
 
 const connURL = `mongodb://localhost:27017/task-manager-api`
 
@@ -9,34 +10,73 @@ mongoose.connect(connURL, {
 
 const User = mongoose.model(`User`,{
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 4,
+        maxlength: 14
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 7,
+        validator(value){
+            if(value.toLowerCase()===`password`){
+                throw new Error(`Arey Howley, password can't contain "password". Please enter a valid password!!`)
+            }
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validator(value) {
+            if(!validator.isEmail(value)){
+                throw new Error(`Provided Email is Invalid`)
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        min: 0
     }
 })
 
 const Task = mongoose.model(`Task`,{
     title: {
-        type: String
+        type: String,
+        required: true
     },
+    description: {
+        type: String,
+        trim: true,
+        required: true,
+
+    },  
     comments: {
-        type: String
+        type: String,
+        required: false
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     }
 })
 
-const me = new User({
-    name: `HeroAB`
+const user = new User({
+    name: `Poppy`,
+    password: `HeroABHI`,
+    email: `poppy@popp.pop`
 })
 
 const code = new Task({
-    title: `complete Task App in 15hrs`,
-    comments: `didn't do any productive work yet.`,
-    completed: false
+    title: `resolving issues in new config`,
+    description: `new config is in hold. we'll start working with it shortly..`,
+    completed: true
 })
 
-me.save()
-code.save()
+user.save().then(()=>console.log(user)).catch((err)=>console.log(err))
+code.save().then(()=>console.log(code)).catch(err=>console.log(err))
